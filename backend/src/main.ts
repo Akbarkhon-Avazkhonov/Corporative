f27e894c3bf854276010cf023a1e573a34bccd10
Akbarkhon <akbarkhonavazkhonov@gmail.com>
2024-07-19 19:42:41 +0500

    4000

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.enableCors();
-  await app.listen(3002);
+  await app.listen(4000);
 }
 bootstrap();
