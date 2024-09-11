fa3d96093e67d6d7c54ce57b159b848d9bd93672
Akbarkhon <akbarkhonavazkhonov@gmail.com>
2024-09-11 16:26:50 +0500

    sendOrderTo1C

--- backend/src/orders/orders.service.ts
+++ backend/src/orders/orders.service.ts
@@ -20,7 +20,12 @@ export class OrdersService {
       createOrderDto.user_id = +link.user_id;
     }
 
-    const order = await this.prisma.orders.create({ data: createOrderDto });
+    const order = await this.prisma.orders.create({
+      data: createOrderDto,
+      include: { Product: true },
+    });
+
+    sendOrderTo1C(order);
 
     return order;
   }
@@ -125,4 +130,52 @@ export class OrdersService {
   remove(id: number) {
     return `This action removes a #${id} order`;
   }
+}
+
+function convertDate(created_at: string | Date) {
+  const date = new Date(created_at);
+  return date.toISOString().split('.')[0].replace('T', ' ');
+}
+async function sendOrderTo1C(order) {
+  const myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'application/json');
+  myHeaders.append(
+    'Authorization',
+    'Basic RXhjaGFuZ2VVc2VyOmZEaUFDQ3JDdm9MTg==',
+  );
+
+  const raw = JSON.stringify({
+    auth: {
+      login: 'ExchangeUser',
+      password: 'oKB0DXgn',
+    },
+    order_id: '1',
+    order_date: convertDate(order.created_at),
+    category: 'partenrs',
+    cpa: 'partners',
+    client_name: order.name + ' ' + order.surname,
+    phone_number: order.phone,
+    webmaster_id: 'webmaster_id',
+    city: order.city,
+    items: {
+      id: order.Product.id,
+      title: order.Product.title,
+      quantity: order.count,
+      PromotionalPrice: order.Product.price,
+    },
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+  };
+
+  await fetch(
+    'https://flashcloud.uz/trade_test2/hs/arbdata/orders/post',
+    requestOptions,
+  )
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.error(error));
 }
