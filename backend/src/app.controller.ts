5a7b4f8d92e8ca016a0e33b898a4993d142c928a
Akbarkhon <akbarkhonavazkhonov@gmail.com>
2024-10-07 00:31:48 +0500

    search test

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ApiTags } from '@nestjs/swagger';
 
@@ -10,5 +10,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('search/:search')
+  search(@Param('search') search: string) {
+    return this.appService.search(search);
   }
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly prisma: PrismaService) {}
   getHello(): string {
     return 'Server is running! ';
+  }
+
+  search(search: string) {
+    return this.prisma.product.findMany({
+      where: {
+        title: {
+          contains: search,
+          mode: 'insensitive',
+        },
+      },
+    });
   }
 }
